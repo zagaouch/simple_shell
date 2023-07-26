2462abb7f6623fbf810cb8ad2104e3210c3fc8ea
zagaouch <i.zagaouch@gmail.com>
2023-07-26 18:29:44 +0100

    task1

--- 1-Simple_shell_1.c
+++ 1-Simple_shell_1.c
@@ -6,51 +6,56 @@ void prompt(void)
 {
 	printf("#cisfun$ ");
 }
+/**
+ * execute_command - function to exec command
+ * @command : the command
+ */
+void execute_command(char* command)
+{
+	pid_t pid = fork();
 
-void execute_command(char* command) {
-    pid_t pid = fork();
+	if (pid < 0)
+	perror("Fork failed");
+	else if (pid == 0)
+	{
+		if (execlp(command, command, NULL) == -1)
+		{
+			perror("Execution failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+	int status;
 
-    if (pid < 0) {
-        perror("Fork failed");
-    } else if (pid == 0) {
-        // Child process
-        if (execlp(command, command, NULL) == -1) {
-            perror("Execution failed");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        // Parent process
-        int status;
-        waitpid(pid, &status, 0);
-    }
+	waitpid(pid, &status, 0);
+	}
 }
+/**
+ * main - the main function
+ * Return: 0
+ */
+int main(void)
+{
+	char *command = NULL;
+	size_t buffer_size = 0;
+	ssize_t characters_read;
+	while (1)
+	{
+		prompt();
 
-int main() {
-    char *command = NULL;
-    size_t buffer_size = 0;
-    ssize_t characters_read;
+		characters_read = getline(&command, &buffer_size, stdin);
 
-    while (1) {
-        prompt();
-
-        characters_read = getline(&command, &buffer_size, stdin);
-
-        if (characters_read == -1) {
-            // Handle "Ctrl+D" (EOF)
-            printf("\n");
-            break;
-        }
-
-        // Remove the newline character from the command
-        command[characters_read - 1] = '\0';
-
-        if (strcmp(command, "exit") == 0) {
-            break;
-        }
-
-        execute_command(command);
-    }
-
-    free(command);
-    return 0;
+		if (characters_read == -1)
+		{
+			printf("\n");
+			break;
+		}
+		command[characters_read - 1] = '\0';
+		if (strcmp(command, "exit") == 0)
+			break;
+		execute_command(command);
+	}
+	free(command);
+	return (0);
 }
