38f120980ee9d7522cc26c5ef202da41fc373260
zagaouch <i.zagaouch@gmail.com>
2023-07-26 07:27:13 -0700

    first task

--- shell.h
+++ shell.h
@@ -1,17 +1,20 @@
-#ifndef HEADER_SHEL
-#define HEADER_SHEL
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-
-void runShell(void);
-void executeCommand(const char *command);
-void printEnvironment(void);
-
-extern char **environ;
-
-
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#define MAX_ARGUMENTS 10
+#define MAX_COMMAND_LENGTH 100
+/* fucntion liste */
+void shell_commande(char *command, char *envp[]);
+void shell_command(char *command, char *arguments[]);
+int main(void);
 #endif
