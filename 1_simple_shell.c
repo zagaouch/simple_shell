52031056165ca0363b97a2677a23a77f373abed8
zagaouch <i.zagaouch@gmail.com>
2023-07-27 11:28:20 +0100

    fix task 6

---
+++ 1_simple_shell.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+/**
+ * shell_commande - function Display a prompt to type a command
+ * @command: the command line
+ * @envp: array of environment variables passed to the program
+ */
+void shell_commande(char *command, char *envp[])
+{
+	{
+	pid_t pid = fork();
+
+	if (pid == 0)
+	{
+		char *argv[2];
+
+		argv[0] = command;
+		argv[1] = NULL;
+
+		execve(command, argv, envp);
+		fprintf(stderr, "Command not found: %s\n", command);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0)
+	{
+		wait(NULL);
+		/*printf("#cisfun$ ");*/
+		fflush(stdout);
+	}
+	else
+	{
+		fprintf(stderr, "ERROR: Fork failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	}
+}
---
+++ main1.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * main_test - execute the commende line
+ * Return 0
+ */
+int main_test(void)
+{
+	char command[MAX_COMMAND_LENGTH];
+
+	printf("$ ");
+	fflush(stdout);
+
+	while (fgets(command, sizeof(command), stdin))
+	{
+		command[strcspn(command, "\n")] = '\0';
+		if (strlen(command) == 0)
+		{
+			printf("$ ");
+			fflush(stdout);
+			continue;
+		}
+		shell_commande(command, NULL);
+		printf("$ ");
+		fflush(stdout);
+	}
+
+	return 0;
+}
